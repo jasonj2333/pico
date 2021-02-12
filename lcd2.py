b45405bfaf3e66b5d53bbeb08217ef0f85f6e422
Jasonj <jasonj2333@gmail.com>
2021-02-12 08:54:03 +0100

    Update lib folder

---
+++ lcd2.py
@@ -0,0 +1,9 @@
+from pico_i2c_lcd import I2cLcd
+from machine import I2C
+from machine import Pin
+import utime as time
+
+i2c = I2C(id=1,scl=Pin(3),sda=Pin(2),freq=100000)
+lcd = I2cLcd(i2c, 0x27, 2, 16) # LCD 16x2
+
+lcd.putstr('Hello World')
